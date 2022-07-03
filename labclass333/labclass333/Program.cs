e4bbedb32d80b401038d355531fb921d1ad53a68
eliyon <55352187+Eliyonqwe@users.noreply.github.com>
2022-07-03 13:09:56 +0300

    .

---
+++ labclass333/labclass333/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labclass333
+{   
+    //internal, { set; get; }, interface(schema provide endiyaderg), 
+   
+    abstract class shape
+    {
+        double CalculateArea();
+        double CalculatePerimeter();
+
+    }
+
+    class circle extends class shape{ 
+        public double radius { get; set; };
+        static double PI = 3.14;
+        double CalculateArea()
+        {
+
+        }
+        double CalculatePerimeter()
+        {
+
+        }
+    }
+
+    class rectange extends class shape
+    {
+        double length;
+        double width;
+        double CalculateArea()
+        {
+
+        }
+        double CalculatePerimeter()
+        {
+
+        }
+    }
+    internal class Program
+   
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
